70592b0aef274e2dfbf7f0aa32df29f04e51240f
ashus-13 <ashutosh.soccer@gmail.com>
2019-03-05 23:11:35 +0530

    some issue with upvote

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('create', views.create,name='create'),
+	path('<int:products_id>', views.detail,name='detail'),
+	path('<int:products_id/upvote>', views.upvote,name='upvote'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Products
 from django.utils import timezone
@@ -11,21 +11,33 @@ def home(request):
 def create(request):
 	if request.method == 'POST':
 		if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
-			product = Products()
-			product.tite = request.POST['title']
-			product.body = request.POST['body']
+			products = Products()
+			products.title = request.POST['title']
+			products.body = request.POST['body']
 			if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
-				product.url = request.POST['url']
+				products.url = request.POST['url']
 			else:
-				product.url = 'http://'	+ request.POST['url']
-			product.icon = request.FILES['icon']
-			product.image = request.FILES['image']
-			product.pub_date = timezone.datetime.now()
-			product.hunter = request.user
-			product.save()
-			return redirect('home')
+				products.url = 'http://'	+ request.POST['url']
+			products.icon = request.FILES['icon']
+			products.image = request.FILES['image']
+			products.pub_date = timezone.datetime.now()
+			products.hunter = request.user
+			products.save()
+			return redirect('/products/'+str(products.id))
 		else:
 			return render(request, 'products/create.html', {'error':'All fields are required'})	
 
 	else:
 		return render(request, 'products/create.html')
+
+def detail(request, products_id):
+	products = get_object_or_404(Products, pk=products_id)
+	return render(request, 'products/detail.html',{'products':products})
+
+
+def upvote(request, products_id):
+	if request.method == 'POST':
+		products = get_object_or_404(Products, pk=products_id)
+		products.tvotes +=1
+		products.save()
+		return redirect('/products/'+str(products.id))

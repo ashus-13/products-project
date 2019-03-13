f7363d882acacf26fe7bf451e99684252c37c731
ashus-13 <ashutosh.soccer@gmail.com>
2019-03-13 10:24:32 +0530

    Solved upvote

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
 	path('create', views.create,name='create'),
 	path('<int:products_id>', views.detail,name='detail'),
-	path('<int:products_id/upvote>', views.upvote,name='upvote'),
+	path('<int:products_id>/upvote', views.upvote,name='upvote'),
 ]
